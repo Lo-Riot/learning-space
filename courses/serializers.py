b27a593c130e6a68312af9823f74ae382b82d6d3
LoRiot <lo_riot@riseup.net>
2022-12-03 19:57:25 +0200

    Create CourseSerializer

---
+++ courses/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from courses.models import Course
+
+
+class CourseSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=50)
+    description = serializers.CharField(max_length=250)
+    rating = serializers.IntegerField(read_only=True)
+    image = serializers.ImageField(required=False)
+    creator = serializers.IntegerField(read_only=True)
+
+    def create(self, validated_data):
+        return Course.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get(
+            'description', instance.description
+        )
+        instance.save()
+        return instance
