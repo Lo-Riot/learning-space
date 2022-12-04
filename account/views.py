da35f9d586b6fdc7bf73e6cc1db6c48993a46b40
LoRiot <lo_riot@riseup.net>
2022-12-04 19:53:56 +0200

    Add generic user views

--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import generics, status
 
-# Create your views here.
+from account.models import User
+from account.serializers import UserSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
