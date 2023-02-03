941a6444d63bd51fbee68ed4c115b84f9b83afbd
LoRiot <lo_riot@riseup.net>
2023-02-03 15:48:07 +0200

    Implement POST method for UserList view

--- account/views.py
+++ account/views.py
@@ -12,6 +12,13 @@ class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
