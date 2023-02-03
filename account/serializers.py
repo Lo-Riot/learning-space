1f7daf71e2f1ccabea0f9d9cc4c56c0efc7f447f
LoRiot <lo_riot@riseup.net>
2023-02-03 15:43:05 +0200

    Rewrite serializers

--- account/serializers.py
+++ account/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, Creator
+from .models import User, Author
 
 
 class UserSerializer(serializers.Serializer):
@@ -8,12 +8,23 @@ class UserSerializer(serializers.Serializer):
     password = serializers.CharField(write_only=True)
     courses = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
 
-class CreatorSerializer(serializers.ModelSerializer):
+    def update(self, instance, validated_data):
+        # TODO: The user should receive an e-mail to confirm password reset
+        instance.password = validated_data.get('password', instance.password)
+
+
+class AuthorSerializer(serializers.Serializer):
     id = serializers.IntegerField(source='user.id', read_only=True)
     username = serializers.CharField(source='user.username', read_only=True)
+    password = serializers.CharField(write_only=True)
     courses = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
-    class Meta:
-        model = Creator
-        fields = ['id', 'username', 'courses']
+    def create(self, validated_data):
+        return Author.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        # TODO: The author should receive an e-mail to confirm password reset
+        instance.password = validated_data.get('password', instance.password)
