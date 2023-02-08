7c69076fbfe37e931d1329621752cf36fdce47e1
LoRiot <lo_riot@riseup.net>
2023-02-08 12:56:37 +0200

    Check if the user is an author

--- courses/permissions.py
+++ courses/permissions.py
@@ -1,10 +1,14 @@
 from rest_framework import permissions
 from account.models import Author
-from loguru import logger
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return Author.objects.filter(user=request.user).exists()
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.author == Author.objects.get(user_id=request.user)
+        return obj.author == Author.objects.get(user=request.user)
