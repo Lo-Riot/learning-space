f410d05465c1106d84de93b2c37babe71c90f041
LoRiot <lo_riot@riseup.net>
2023-02-08 12:24:05 +0200

    Refactor courses tests using setUpClass and force_login

--- courses/tests.py
+++ courses/tests.py
@@ -4,35 +4,43 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from account.models import User, Author
-from courses.models import Course
-from courses.serializers import CourseSerializer
+from courses.models import Course, Lesson
+from courses.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseCreateTestCase(APITestCase):
-    def setUp(self):
-        user_data = {'username': "TestUser", 'password': "test"}
-        user = User.objects.create_user(**user_data)
-        author = Author.objects.create(user=user)
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        user = User.objects.create_user(
+            username="TestUser",
+            password="test"
+        )
+        cls.author = Author.objects.create(user=user)
+        cls.unauth_user = User.objects.create_user(
+            username="TestUnauthUser",
+            password="test"
+        )
 
-        self.user_cookies = self.client.post(
-            reverse('login'), data=user_data
-        ).cookies
+    def setUp(self):
         self.course_data = {
             'name': "TestCourse",
             'description': "Course description",
-            'author': author.pk
         }
 
     def test_course_create_without_image(self):
+        self.client.force_login(self.author.user)
+
         response = self.client.post(
             reverse('courses'),
             data=self.course_data,
-            cookies=self.user_cookies
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['author'], self.course_data['author'])
+        self.assertEqual(response.data['author'], self.author.pk)
 
     def test_course_create_with_image(self):
+        self.client.force_login(self.author.user)
+
         with open(settings.MEDIA_ROOT / "uploads/test.jpg", 'rb') as image:
             updated_course_data = {'image': image}
             updated_course_data.update(self.course_data)
@@ -40,28 +48,37 @@ class CourseCreateTestCase(APITestCase):
             response = self.client.post(
                 reverse('courses'),
                 data=updated_course_data,
-                cookies=self.user_cookies
             )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['author'], self.course_data['author'])
+        self.assertEqual(response.data['author'], self.author.pk)
+
+    def test_course_create_unauth(self):
+        self.client.force_login(self.unauth_user)
+
+        response = self.client.post(
+            reverse('courses'),
+            data=self.course_data,
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
 class CourseTestCase(APITestCase):
-    def setUp(self):
-        user_data = {'username': "TestUser", 'password': "test"}
-        user = User.objects.create_user(**user_data)
-        self.author = Author.objects.create(user=user)
-
-        self.user_cookies = self.client.post(
-            reverse('login'), data=user_data
-        ).cookies
-        self.course_data = {
-            'name': "TestCourse",
-            'description': "Course description",
-        }
-        self.course = Course.objects.create(
-            **self.course_data, author=self.author
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        user = User.objects.create_user(
+            username="TestUser",
+            password="test"
         )
+        cls.author = Author.objects.create(user=user)
+        cls.course = Course.objects.create(
+            name="TestCourse",
+            description="Course description",
+            author=cls.author,
+        )
+
+    def setUp(self):
+        self.client.force_login(self.author.user)
         self.serializer = CourseSerializer(self.course)
 
     def test_course_list(self):
@@ -70,32 +87,99 @@ class CourseTestCase(APITestCase):
 
     def test_course_detail(self):
         response = self.client.get(reverse('course', args=[self.course.pk]))
-        author = Author.objects.get(pk=response.data['author'])
-
         self.assertEqual(response.data, self.serializer.data)
-        self.assertEqual(
-            CourseSerializer(author.course_set.all()[0]).data, response.data
-        )
 
     def test_course_update(self):
-        updated_course_data = {
-            'name': "TestCourse",
-            'description': "Description is updated",
-        }
-
-        self.assertNotEqual(
-            self.course.description,
-            updated_course_data['description']
-        )
-        self.course.description = updated_course_data['description']
+        self.course.description = "Description is updated"
 
         response = self.client.put(
             reverse('course', args=[self.course.pk]),
-            data=updated_course_data,
-            cookies=self.user_cookies
+            data={'name': "TestCourse", 'description': self.course.description}
         )
         self.assertEqual(response.data, self.serializer.data)
 
     def test_course_delete(self):
         response = self.client.delete(reverse('course', args=[self.course.pk]))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class LessonCreateTestCase(APITestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        user = User.objects.create_user(
+            username="TestUser",
+            password="test"
+        )
+        cls.author = Author.objects.create(user=user)
+        cls.course = Course.objects.create(
+            name="TestCourse",
+            description="Course description",
+            author=cls.author,
+        )
+        cls.lesson_data = {
+            'name': "TestLesson",
+            'content': "Lesson content",
+        }
+
+    def setUp(self):
+        self.client.force_login(self.author.user)
+
+    def test_lesson_create(self):
+        response = self.client.post(
+            reverse('lessons', args=[self.course.pk]),
+            data=self.lesson_data,
+        )
+        self.assertEqual(
+            response.status_code, status.HTTP_201_CREATED, msg=response.data
+        )
+
+
+class LessonTestCase(APITestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        user = User.objects.create_user(
+            username="TestUser",
+            password="test"
+        )
+        cls.author = Author.objects.create(user=user)
+        cls.course = Course.objects.create(
+            name="TestCourse",
+            description="Course description",
+            author=cls.author,
+        )
+        cls.lesson = Lesson.objects.create(
+            name="TestLesson",
+            content="Lesson content",
+            course=cls.course,
+        )
+
+    def setUp(self):
+        self.client.force_login(self.author.user)
+        self.serializer = LessonSerializer(self.lesson)
+
+    def test_lesson_list(self):
+        response = self.client.get(reverse('lessons', args=[self.course.pk]))
+        self.assertEqual(response.data, [self.serializer.data])
+
+    def test_lesson_detail(self):
+        response = self.client.get(reverse(
+            'lesson', args=[self.course.pk, self.lesson.pk]
+        ))
+        self.assertEqual(response.data, self.serializer.data)
+
+    def test_lesson_update(self):
+        self.lesson.content = "Updated lesson content"
+
+        response = self.client.put(
+            reverse('lesson', args=[self.course.pk, self.lesson.pk]),
+            data={'name': "TestLesson", 'content': self.lesson.content},
+        )
+        self.assertEqual(response.data, self.serializer.data)
+
+    def test_course_delete(self):
+        response = self.client.delete(reverse(
+            'lesson', args=[self.course.pk, self.lesson.pk]
+        ))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
